3508a790a5761bcd336057a6c86112fc3e1c3714
Mahmod94 <78415546+Mahmod94@users.noreply.github.com>
2026-02-04 13:53:36 +0100

    Update NavBar.tsx

--- FocusFlow/src/components/NavBar.tsx
+++ FocusFlow/src/components/NavBar.tsx
@@ -1,24 +1,40 @@
-import { AppBar, Toolbar, Stack} from "@mui/material";
+import { Box, AppBar, Toolbar, Stack, Typography} from "@mui/material";
 import { Link } from "react-router-dom";
 
+import { useState } from "react";
+
 export default function NavBar(){
+
+    const [ancorEl, setAncorEl] = useState<null | HTMLElement>(null);
+    const 
+
+
     return (
-        <AppBar position="static">
-            <Toolbar>
-                <Stack direction="row" spacing={3}>
-                    <Link to="/" style={{ color: "white", textDecoration: "none"}}>
-                    Tasks
-                    </Link>
+        <Box sx={{ flexGrow: 1}}>
+            <AppBar position="fixed" color="secondary">
+                <Toolbar variant="dense">
+                    <Stack direction="row" spacing={3}>
+                        <Link to="/" style={{ color: "inhert", textDecoration: "none"}}>
+                        <Typography variant="h6" component="div" sx={{ flexGrow: 1}}>Tasks</Typography>
+                        </Link>
 
-                    <Link to="/promodoropage" style={{ color: "white", textDecoration:"none"}}>
-                    Promodoro
-                    </Link>
+                        <Link to="/promodoropage" style={{ color: "inherit", textDecoration:"none"}}>
+                        <Typography variant="h6" component="div" sx={{ flexGrow: 1}}>
+                            Promodoro
+                            </Typography>
+                        </Link>
 
-                    <Link to="/statspage" style={{color: "white", textDecoration: "none"}}>
-                    Stats
-                    </Link>
-                </Stack>
-            </Toolbar>
-        </AppBar>
-    )
+                        <Link to="/statspage" style={{color: "inherit", textDecoration: "none"}}>
+                        <Typography variant="h6" component="div" sx={{ flexGrow: 1}}>
+                            Stats
+                        </Typography>
+                        <Typography variant="h6" component="div" sx={{ flexGrow: 1}}>
+                            Login
+                        </Typography>
+                        </Link>
+                    </Stack>
+                </Toolbar>
+            </AppBar>
+        </Box>
+    );
 }
