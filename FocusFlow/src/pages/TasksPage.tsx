1b760f49d8dd1c82fc61134d22eb88dbff91fee2
Mahmod94 <78415546+Mahmod94@users.noreply.github.com>
2026-02-09 12:43:28 +0100

    Update TasksPage.tsx

--- FocusFlow/src/pages/TasksPage.tsx
+++ FocusFlow/src/pages/TasksPage.tsx
@@ -1,49 +1,4 @@
-// import { useState } from "react";
-// import type { Task } from "../types/task";
-// import { Typography } from "@mui/material";
 
-
-// type Props = {
-//   tasks: Task[];
-//   setTasks: React.Dispatch<React.SetStateAction<Task[]>>;
-// }
-
-// export default function TasksPage({ tasks, setTasks} : Props) {
-
-//   const [name, setName] = useState("");
-
-//   const handleAdd = () => {
-//     const title = name.trim();
-//     if (!title) return;
-
-//     const newTask: Task = { id: crypto.randomUUID(), title, status: "todo" };
-//     setTasks((prev) => [...prev, newTask]);
-//     setName("");
-//   };
-
-//   const handleDelete = (id: string) => {
-//     setTasks((prev) => prev.filter((t) => t.id !== id));
-//   };
-
-//   return (
-//     <>
-//       <Typography variant="h4" component="h2" sx={{ color: "#C45AB3"}}>Tasks</Typography>
-//       <input value={name} onChange={(e) => setName(e.target.value)} />
-//       <button type="button" onClick={handleAdd}>Add</button>
-
-//       <ul>
-//         {tasks.map((t) => (
-//           <li key={t.id}>
-//             {t.title}{" "}
-//             <button type="button" onClick={() => handleDelete(t.id)}>
-//               delete
-//             </button>
-//           </li>
-//         ))}
-//       </ul>
-//     </>
-//   );
-// }
 
 import { useMemo, useState } from "react";
 import { Button, Checkbox, TextField, Typography } from "@mui/material";
